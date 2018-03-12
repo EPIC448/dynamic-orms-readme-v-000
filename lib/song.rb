fb330e21f40e211d2a890b2c8eb43c91af7802ea
Sam. O <samrey2018@gmail.com>
2018-03-12 22:26:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-
+require 'pry'
 class Song
 
   def self.table_name
     self.to_s.downcase.pluralize
+    
   end
 
   def self.column_names
